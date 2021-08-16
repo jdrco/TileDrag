79ad8368c97eae66d8fb9f10199d988b04229715
Jared Drueco <drueco03@gmail.com>
2021-08-15 22:55:26 -0600

    Centered everything. Created media query to adapt to laptop screen

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // Board/Player/Movement-logic creation -------------------------------------------
     function initialize() {
 
         var audio = new Audio('./sounds/move1.mp3')
         //audio.volume = 0.5;
-        
+
         // Creating the board. Set up the players initial position.
         var board = document.querySelector('#board');
         for (let i = 0; i < 8; i++) {
             for (let j = 0; j < 8; j++) {
                 var indicator = i + j;
-                
+
                 if (indicator % 2 != 0) {
                     // render a black tile
                     var tile = document.createElement('div');
                     tile.id = i + ',' + j;
                     tile.className = 'tile black-tile';
                     board.appendChild(tile);
-                     
+
                     // render player
                     if (i == 4 && j == 3) {
-    
+
                         let player = document.createElement("div");
                         player.className = 'player';
                         document.getElementById('4,3').appendChild(player);
                     }
-                    
-                }
-                else {
+
+                } else {
                     // render a white tile
                     var tile = document.createElement('div');
                     tile.id = i + ',' + j;
@@ -42,22 +40,22 @@ document.addEventListener('DOMContentLoaded', function() {
         var player = document.querySelector('.player');
         // get the tiles from the board
         var tiles = board.querySelectorAll('.tile');
-        
+
         // Player Listeners
         document.addEventListener('mousedown', dragstart);
         document.addEventListener('mousemove', dragmove);
         document.addEventListener('mouseup', dragdrop);
-    
+
         // tile hover on while mouse is down on player
         // Reference: https://stackoverflow.com/questions/34483940/best-way-to-run-mousemove-only-on-mousedown-with-pure-javascript
-        player.addEventListener("mousedown", function(e){
+        player.addEventListener("mousedown", function (e) {
             // call the mousemove function
-            player.onmousemove = function(e) {
+            player.onmousemove = function (e) {
                 mouseMove(e);
             }
         });
-        
-        player.addEventListener("mouseup", function(e){
+
+        player.addEventListener("mouseup", function (e) {
             for (const tile of tiles) {
                 if (tile.classList.contains('hover')) {
                     tile.classList.remove('hover')
@@ -65,12 +63,12 @@ document.addEventListener('DOMContentLoaded', function() {
             }
             player.onmousemove = null;
         });
-        
+
         function mouseMove(e) {
-            const j = Math.floor((e.clientX - board.offsetLeft) / 100); 
-            const i = Math.floor((e.clientY - board.offsetTop) / 100);
+            const j = Math.floor((e.clientX - board.offsetLeft) / 80);
+            const i = Math.floor((e.clientY - board.offsetTop) / 80);
             var id = i + "," + j;
-    
+
             if (document.getElementById(id)) {
                 document.getElementById(id).classList.add('hover')
                 for (const tile of tiles) {
@@ -78,39 +76,33 @@ document.addEventListener('DOMContentLoaded', function() {
                         tile.classList.remove('hover')
                     }
                 }
-            } else { // handle the case where mouse isn't on board
-                for (const tile of tiles) {
-                    if (tile.classList.contains('hover')) {
-                        tile.classList.remove('hover')
-                    }
-                }
             }
         }
-        
+
         // initialize active player as none
-	let activePiece = null;
-    
+        let activePiece = null;
+
         // drag functions
         // Center the player to the mouse position on mousdown
         function dragstart(e) {
-    
-	    const element = e.target;
+
+            const element = e.target;
             if (element.classList.contains('player')) {
-                
+
                 element.style.position = 'absolute';
-                var x = e.clientX - 50;
-                var y = e.clientY - 48;
+                var x = e.clientX - 39;
+                var y = e.clientY - 38;
                 element.style.left = x + "px";
                 element.style.top = y + "px";
-                
+
                 activePiece = element;
                 // console.log('current ' + activePiece.parentNode.id);
             }
         }
-    
+
         // move the piece wherever the mouse is while the mouse is still down
         function dragmove(e) {
-    
+
             if (activePiece) {
                 // constrain player to board while moving the piece to current mouse position
                 activePiece.style.position = 'absolute';
@@ -118,18 +110,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 const minY = board.offsetTop - 20;
                 const maxX = board.offsetLeft + board.clientWidth - 80;
                 const maxY = board.offsetTop + board.clientHeight - 80;
-                var x = e.clientX - 50; 
-                var y = e.clientY - 48;
-    
-                if  (x < minX) {
+                var x = e.clientX - 39;
+                var y = e.clientY - 38;
+
+                if (x < minX) {
                     activePiece.style.left = minX + "px";
                 } else if (x > maxX) {
                     activePiece.style.left = maxX + "px";
                 } else {
                     activePiece.style.left = x + "px";
                 }
-    
-                if  (y < minY) {
+
+                if (y < minY) {
                     activePiece.style.top = minY + "px";
                 } else if (y > maxY) {
                     activePiece.style.top = maxY + "px";
@@ -141,43 +133,49 @@ document.addEventListener('DOMContentLoaded', function() {
                 //     (x < minX)
                 //         ? minX + "px" // true case (if)
                 //         : x + "px";   // false case (else)
-    
+
                 // activePiece.style.top = 
                 // (y < minY)
                 //     ? minY + "px"
                 //     : y + "px";
             }
         }
-    
+
         // drop the piece when the mouse is up
         function dragdrop(e) {
-    
+
             if (activePiece) {
-    
+                // handle the case where mouse isn't on board
+                for (const tile of tiles) {
+                    if (tile.classList.contains('hover')) {
+                        tile.classList.remove('hover')
+                    }
+                }
+
                 var player = document.querySelector('.player');
-                
+
                 // get the coordinates of the mousedrop wrt to board coordinates
-                const j = Math.floor((e.clientX - board.offsetLeft) / 100); 
-                const i = Math.floor((e.clientY - board.offsetTop) / 100);
-    
+                const j = Math.floor((e.clientX - board.offsetLeft) / 80);
+                const i = Math.floor((e.clientY - board.offsetTop) / 80);
+
                 // prevent player from dropping to a non-real tile (outside of board)
                 if (j < 0) {
                     activePiece.style.position = 'static';
                     activePiece = null;
-    
-                } else if (j > 7){
+
+                } else if (j > 7) {
                     activePiece.style.position = 'static';
                     activePiece = null;
-    
-                } else if ( i < 0){
+
+                } else if (i < 0) {
                     activePiece.style.position = 'static';
                     activePiece = null;
-    
-                } else if (i > 7){
+
+                } else if (i > 7) {
                     activePiece.style.position = 'static';
                     activePiece = null;
-                
-                // Regular case (player drops it on board)
+
+                    // Regular case (player drops it on board)
                 } else {
                     var id = i + "," + j;
                     if (id != e.target.parentNode.id) {
@@ -188,9 +186,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     document.getElementById(e.target.parentNode.id).removeChild(player);
                     document.getElementById(id).appendChild(player);
                     activePiece.style.position = 'static';
-    
+
                     activePiece = null;
-                }   
+                }
             }
         }
     }
@@ -207,31 +205,44 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var player = document.querySelector('.player');
     var tiles = board.querySelectorAll('.tile');
-    var gamestart = false;
-    
-    var score = document.querySelector('.score');
 
-    startbutton.addEventListener('click', gameon, {once: false});
-    
+    var score = document.querySelector('.score');
+    var counter = 0;
+    score.innerHTML = counter;
+
+    var counting = document.querySelector('.time');
+    var seconds = 30;
+    counting.innerHTML = seconds + "s";
+    var countingdown;
+
+    startbutton.addEventListener('click', gameon, {
+        once: false
+    });
+
     function gameon() {
         console.clear();
         clearalltiles();
+        reset();
+        starttime();
 
         // clear last intervals when player wants to start a new game
         clearInterval(count);
         clearInterval(cycle);
         // count down before starting to pick random tiles
         countdown(chooserandomtile);
-
         checktargethit(player);
-        gameover();
+    }
+
+    function reset() {
+        counter = 0;
+        score.innerHTML = counter;
     }
 
     function countdown(callback) {
         timer.innerHTML = "3"
         timer.classList.remove('hide');
         var time = 3;
-        count = setInterval(function() {
+        count = setInterval(function () {
             time--;
             if (time > 0) {
                 console.log(time)
@@ -241,7 +252,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 timer.innerHTML = null;
                 clearInterval(count);
                 callback();
-            } 
+            }
         }, 1000);
     }
 
@@ -255,10 +266,23 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                 tiles[tile].classList.add('target');
             }
-            
+
         }, interval);
     }
 
+    function starttime(gameover) {
+        setTimeout(() => {
+            clearInterval(countingdown);
+            countingdown = setInterval(() => {
+                seconds--;
+                counting.innerHTML = seconds + "s";
+                if (seconds == 0) {
+                    clearInterval(countingdown);
+                    gameover();
+                }
+            }, 1000)
+        }, 3000)
+    }
 
     function clearalltiles() {
         for (const tile of tiles) {
@@ -268,11 +292,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function checktargethit(player) {
         player.addEventListener('mouseup', (e) => {
-            const j = Math.floor((e.clientX - board.offsetLeft) / 100); 
-            const i = Math.floor((e.clientY - board.offsetTop) / 100);
+            const j = Math.floor((e.clientX - board.offsetLeft) / 80);
+            const i = Math.floor((e.clientY - board.offsetTop) / 80);
             var id = i + "," + j;
             if (document.getElementById(id) && document.getElementById(id).classList.contains('target')) {
                 document.getElementById(id).classList.remove('target');
+                counter++;
+                score.innerHTML = counter;
             }
         });
     }
@@ -282,6 +308,6 @@ document.addEventListener('DOMContentLoaded', function() {
             tile.classList.remove('target');
         }
     }
-    
+
 
 });
